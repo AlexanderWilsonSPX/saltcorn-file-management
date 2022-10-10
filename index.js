c17b9654c4ad6fb52ac50267b5bfe57d14cc445d
Alexander Wilson <Alexander.Wilson@spx.com>
2022-10-10 13:40:37 -0400

    Added readFile and writeFile functions.

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
-const file = require("@saltcorn/data/models/file");
+const File = require("@saltcorn/data/models/file");
+const db = require("@saltcorn/data/db");
 const fs = require("fs");
 
+const readFile = {
+    run: async (name) => {
+        const f = await File.findOne({"filename": name});
+        if(f != null){
+            return fs.readFileSync(f.location)
+        }else{
+            return "";
+        }
+    },
+    isAsync: true,
+};
+
+const writeFile = {
+    run: async (name, data) => {
+        const f = await File.findOne({"filename": name});
+
+        var path = null;
+
+        if(f != null){
+            path = f.location;
+            fs.writeFileSync(path, data);
+        }else{
+            // Create a new file in the file store path
+            const path = db.connectObj.file_store + "/";
+            fs.writeFileSync(path + name, data);
+
+            // Have saltcorn give it a UUID and register it
+            await File.from_existing_file(path, name, 1);   // ToDo: Get the user ID
+        }
+    },
+    isAsync: true,
+}
+
 module.exports = {
-  sc_plugin_api_version: 1,
-  functions: { filetest: (m) => console.log(m) },
+    sc_plugin_api_version: 1,
+    functions: { readFile, writeFile },
 };
 
